8729451e5d517a9ce0e2eddef2f0deb2bce89424
Tomasz Drabik <tomasz.drabik@creativetyle.pl>
2022-02-22 13:15:17 +0100

    init empty

--- registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'MageSuite_GoogleOptimizeIntegration',
-    __DIR__
-);
